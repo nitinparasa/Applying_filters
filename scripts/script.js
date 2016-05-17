d7d6c07813f4d723dca9b6946b5cd831a5637abf
Nitin Parasa <nitin.parasa@yahoo.com>
2016-05-17 09:04:33 +0200

    Navigation Bar Added

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by NitinPC on 11-05-2016.
+ */
+/// <reference path="angular.min.js" />
+var myApp = angular
+                .module("myApp", [])
+                .controller("mainController", function($scope){
+                    $scope.name="Please enter your username";
+                    $scope.email="Please enter your email address";
+});
