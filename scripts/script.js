9fde062a5f625c2dac577438b35630dd29960692
Nitin Parasa <nitin.parasa@yahoo.com>
2016-05-24 09:16:00 +0200

    changes made to the index page

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,11 @@
  * Created by NitinPC on 11-05-2016.
  */
 /// <reference path="angular.min.js" />
-var myApp = angular
+/*var myApp = angular
                 .module("myApp", [])
                 .controller("mainController", function($scope){
                     $scope.name="Please enter your username";
                     //$scope.email="Please enter your email address";
-});
+});*/
+var app = angular.module('app_filters', [])
+                 .controller("tabscontroller", function(){});
