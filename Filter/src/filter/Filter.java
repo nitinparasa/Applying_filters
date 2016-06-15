702a4b2e15f363e76ffe6f75e0401c3203934f3d
mahith <k.mahith@gmail.com>
2016-06-15 17:10:03 +0200

    main class(cmd error)

--- Filter/src/filter/Filter.java
+++ Filter/src/filter/Filter.java
@@ -17,6 +17,8 @@ public class Filter {
    public static void main(String[] args) {
         // TODO code application logic here
       System.out.println("");
+      
+      new Login().setVisible(true);
      
     }
     
